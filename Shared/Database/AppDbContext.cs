e9e0bc474dd141c69d5157f4e0dc9a42a99df013
Admin <Admin@SLO>
2024-04-17 21:49:20 +0200

    Add project files.

---
+++ Shared/Database/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Shared.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<tblTopic> tblTopics { get; set; }
+
+        public AppDbContext()
+        {
+            SQLitePCL.Batteries_V2.Init();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(FileSystem.AppDataDirectory, "Database.db3");
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+
+
+    
+    }
+
+}
---
+++ Try1/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Shared.Database;
+
+namespace Try1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            // Automatically create the database if it doesn't exist and apply migrations at runtime
+            builder.Services.AddDbContext<AppDbContext>();
+      
+
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
